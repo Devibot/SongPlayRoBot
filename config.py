24ef8146e233149dd95368144c36b958f91ffef6
Devibot <84560159+Devibot@users.noreply.github.com>
2021-06-20 12:24:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "4893645"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "b987161603a4237beec2528c6b66ca87" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "1740417443:AAGRW7Z1KB6FNX-yvw1uVH6pcRz0gW95dtc" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
